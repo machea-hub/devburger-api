c95ee7e73b19a2b5de54c7df83a604acfcecadfd
machea-hub <lucas.machea@hotmail.com>
2025-10-02 20:09:28 -0300

    yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,30 @@
 import { v4 } from "uuid";
+import * as Yup from "yup";
 
 import User from "../models/User.js";
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean(),
+        });
+
+        try {
+            schema.validateSync(req.body, { abortEarly: false });
+        } catch (error) {
+            return res.status(400).json({ error: error.errors });           
+        }
+
         const { name, email, password_hash, admin} = req.body;
+
+        const userExists = await User.findOne({
+            where: {
+                email,
+            }
+        })
 
         const user = await User.create({
             id: v4(),

aedfbeb2ce7a3264e9f4bcbc947052c8aab383ee
machea-hub <lucas.machea@hotmail.com>
2025-10-02 18:44:41 -0300

    user_controller

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,8 @@
 import { Router } from "express";
-import { v4 } from "uuid";
-
-import User from "./app/models/User.js";
-import { password } from "./config/database";
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get("/", async(req, res) => {
-    const user = await User.create({
-        id: v4(),
-        name: "Lucas",
-        email: "Lucas2@email.com",
-        password_hash: "123456"
-    });
-
-    return res.status(200).json(user);
-})
+routes.post("/users", UserController.store);
 
 export default routes;

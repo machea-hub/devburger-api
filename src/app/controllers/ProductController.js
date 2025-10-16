51edc400f8f221f9bb03e1b97d65378359c712b4
machea-hub <lucas.machea@hotmail.com>
2025-10-16 15:52:04 -0300

    products_controller

---
+++ src/app/controllers/ProductController.js
@@ -0,0 +1,21 @@
+import * as Yup from 'yup';
+
+class ProductController {
+  async store(req, res) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      price: Yup.number().required(),
+      category: Yup.string().required(),
+    });
+
+    try {
+      schema.validateSync(req.body, { abortEarly: false, strict: true });
+    } catch (err) {
+      return res.status(400).json({ error: err.errors });
+    }
+
+    return res.status(201).json({ ok: true });
+  }
+}
+
+export default new ProductController();

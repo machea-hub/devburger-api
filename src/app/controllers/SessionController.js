de155edf1dcef46606f2bc4ab70951fb5063f74b
machea-hub <lucas.machea@hotmail.com>
2025-10-16 15:14:32 -0300

    login

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,52 @@
+import * as Yup from 'yup';
+import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+
+class SessionController {
+  async store(req, res) {
+    const schema = Yup.object({
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+    });
+
+    const isValid = await schema.isValid(req.body, { strict: true });
+
+    const eopi = () => {
+      return res.status(400).json({ error: 'Email/password incorrect' });
+    };
+
+    if (!isValid) {
+      eopi();
+    }
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!existingUser) {
+      eopi();
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password_hash,
+    );
+
+    if (!isPasswordCorrect) {
+      eopi();
+    }
+
+    return res.status(200).json({
+      id: existingUser.id,
+      name: existingUser.name,
+      email: existingUser.email,
+      admin: existingUser.admin,
+    });
+  }
+}
+
+export default new SessionController();

f0ca01a98faf5bfd76d729a9b926314921d8d59d
machea-hub <lucas.machea@hotmail.com>
2025-10-23 13:10:12 -0300

    globalStyles_config

--- devburguer-interface/src/main.jsx
+++ devburguer-interface/src/main.jsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GlobalStyles from './styles/globalStyles.js';
 import { Login } from './containers/Login';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Login />
-  </StrictMode>,
+    <GlobalStyles />
+  </React.StrictMode>,
 );

2c6c7aec5fc01da1523733adbb3521a732573db6
machea-hub <lucas.machea@hotmail.com>
2025-10-22 22:44:31 -0300

    put_index_orders

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 import Product from '../models/Product.js';
 import Category from '../models/Category.js';
-import Order from '../schemas/Order.js'
+import Order from '../schemas/Order.js';
 
 class OrderController {
   async store(request, response) {
@@ -62,9 +62,40 @@ class OrderController {
       status: 'Pedido realizado',
     };
 
-    const newOrder = await Order.create(order)
+    const newOrder = await Order.create(order);
 
     return response.status(201).json(newOrder);
+  }
+
+  async update(request, response) {
+    const schema = Yup.object({
+      status: Yup.string().required(),
+    });
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false, strict: true });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    const { status } = request.body;
+    const { id } = request.params;
+
+    try {
+      await Order.updateOne({ _id: id }, { status });
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+
+    return response
+      .status(200)
+      .json({ message: 'Status updated successfully' });
+  }
+
+  async index(_request, response) {
+    const orders = await Order.find();
+
+    return response.status(200).json(orders);
   }
 }
 

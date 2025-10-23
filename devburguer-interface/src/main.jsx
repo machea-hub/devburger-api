818ea319f3b45390b4074daef46033b6e62ace3c
machea-hub <lucas.machea@hotmail.com>
2025-10-23 12:49:02 -0300

    eslint-prettier_configs

--- devburguer-interface/src/main.jsx
+++ devburguer-interface/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Login } from './containers/Login'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Login } from './containers/Login';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login />
   </StrictMode>,
-)
+);
